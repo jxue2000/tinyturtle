851f178d7b0c49db050b3d8389e24bd01da855de
Jennifer Xue <jxue@aclanyc.com>
2017-03-01 13:24:22 +0000

    made house

---
+++ script.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window);
+
+forward(40);
+right(45);
+forward(40);
+right(90);
+forward(40);
+right(135);
+forward(57);
+right(134);
+forward(41);
+right(91);
+forward(40);
+right(45);
+forward(42);
+right(91);
+forward(57);
